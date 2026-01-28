0e57d871d962672683660df936082f2f1cd40ad6
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-28 14:36:14 -0500

    deleted some whitespace

--- lab1/lab1.java
+++ lab1/lab1.java
@@ -33,21 +33,3 @@ public class lab1 {
         
     }
 }
-
-// 4. Write Java code to perform the following:
-// • Calculate state withholding tax at 3.0% and federal withholding tax at 30.0%
-// • Calculate dependent deductions at 5.0% of the employee’s salary for each dependent.
-// • Calculate total withholding.
-// • Calculate take-home pay as salary minus total withholding plus deductions.
-// • Declare constants for each of the percentages above and assign the values to them.
-// • Remember to convert percentages to decimals.
-// • Use the DecimalFormat class to express the results to two decimal places.
-// 5. Save the source code in a directory of your choice.
-// 6. Compile the program and fix any syntax errors that may exist.
-// 7. Execute the program. You should get the following output:
-// • The State Tax is: $28.50
-// • The Federal Tax is: $285.00
-// • Dependents: $142.50
-// • The Salary is: $950.00
-// • The Take-Home Pay is: $779.00
-// 8. Submit the source code in blackboard for testing
